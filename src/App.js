b1704ee5dff66866db2908ea8626273fd010c6dd
hasan <hasanrizwan6@gmail.com>
2022-02-25 23:38:11 +0500

    Added Local Storage, one error stil needs to grt resolved.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,53 @@ import Alert from "./components/Alert";
 // import About from "./components/About";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('');
+  const [alert, setAlert] = useState('dark');
+  const [btnCheck, setBtnCheck] = useState('');
+  const [check, setChecked] = useState('');
+  let loadMode = () => {
+    let localData = JSON.parse(localStorage.getItem('mode'));
+    if (localData == 'dark') {
+      setMode('dark');
+      showAlert('Dark Mode Has Been Enabled', 'light');
+      document.body.style.backgroundColor = '#212529';
+      document.title = 'Text Utility - Dark Mode';
+      setBtnCheck('flexSwitchCheckChecked');
+      setChecked('checked');
+    }
+    else {
+      setMode('light');
+      showAlert('Light Mode Has Been Enabled', 'dark');
+      document.body.style.backgroundColor = 'white';
+      document.title = 'Text Utility - Light Mode';
+      setBtnCheck('flexSwitchCheckDefault');
+      setChecked('');
+    }
+  }
+  window.onload = function () {
+    loadMode();
+  }
+  const toggleMode = () => {
+    if (mode == 'light') {
+      setMode('dark');
+      showAlert('Dark Mode Has Been Enabled', 'light');
+      document.title = 'Text Utility - Dark Mode';
+      document.body.style.backgroundColor = '#212529';
+      localStorage.setItem('mode', JSON.stringify('dark'));
+      setBtnCheck('flexSwitchCheckChecked');
+      setChecked('checked');
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode Has Been Enabled', 'dark');
+      document.title = 'Text Utility - Light Mode';
+      localStorage.setItem('mode', JSON.stringify('light'));
+      setBtnCheck('flexSwitchCheckDefault');
+      setChecked('');
+    }
+  }
+
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
@@ -15,30 +60,19 @@ function App() {
     })
     setTimeout(() => {
       setAlert(null)
-      
+
     }, 1000);
   }
-  const toggleMode = () => {
-    if (mode == 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = '#212529';
-      showAlert('Dark Mode Has Been Enabled', 'light');
-    }
-    else {
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
-      showAlert('Light Mode Has Been Enabled', 'dark');
-    }
-  }
+
   return (
-    <>
-      <Navbar mode={mode} toggleMode={toggleMode} />
+    <div>
+      <Navbar mode={mode} btnCheck={btnCheck} check={check} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className='container my-3'>
         <Form mode={mode} heading='Enter Text To See The Magic' alert={alert} />
         {/* <About /> */}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 
 export default function Form(props) {
     const [text, setText] = useState('');
+    const [wordCount, setWordCount] = useState(null)
     const uppercaseOnClick = () => {
         let upperCasedText = text.toUpperCase();
         setText(upperCasedText);
     }
-    const uppercaseOnChange = (event) => {
+    const onChange = (event) => {
         setText(event.target.value);
     }
     const lowercaseOnClick = () => {
@@ -23,6 +24,13 @@ export default function Form(props) {
             setText(capitalizedText);
         }
     }
+    const wordCounter = () => {
+        let counter = 0;
+        if (text == '') {
+            // setWordCount(counter);
+            // console.log('no word');
+        }
+    }
     const clearOnClick = () => {
         setText('');
     }
@@ -30,20 +38,21 @@ export default function Form(props) {
         <div>
             <h1 className={`d-sm-flex my-3 text-${props.mode === 'light' ? 'dark' : 'light'}`}>{props.heading}</h1>
             <div className="mb-3">
-                <textarea style={{ backgroundColor: props.mode === 'dark' ? '#212529' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} className="form-control" value={text} onChange={uppercaseOnChange} id="exampleFormControlTextarea1" rows="3" placeholder='Enter Text Here'></textarea>
+                <textarea style={{ backgroundColor: props.mode === 'dark' ? '#212529' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} className="form-control" value={text} onChange={onChange} id="exampleFormControlTextarea1" rows="3" placeholder='Enter Text Here'></textarea>
             </div>
             <div className="d-sm-flex">
-                <button type="button" className={`btn btn-${props.mode === 'light' ? 'primary': 'light'} text-${props.mode === 'light' ? 'white': 'black'}`} onClick={uppercaseOnClick}>Uppercase</button>
-                <button type="button" className={`btn btn-${props.mode === 'light' ? 'success': 'light'} mx-3 text-${props.mode === 'light' ? 'white': 'black'}`} onClick={lowercaseOnClick}>Lowercase</button>
-                <button type="button" className={`btn btn-${props.mode === 'light' ? 'warning': 'light'} text-${props.mode === 'light' ? 'white': 'black'}`} onClick={capitalizeOnClick}>Captialize</button>
-                <button type="button" className={`btn btn-${props.mode === 'light' ? 'danger': 'light'} mx-3 text-${props.mode === 'light' ? 'white': 'black'}`} onClick={clearOnClick}>Clear Text</button>
+                <button type="button" className={`btn btn-${props.mode === 'light' ? 'primary' : 'light'} text-${props.mode === 'light' ? 'white' : 'black'}`} onClick={uppercaseOnClick}>Uppercase</button>
+                <button type="button" className={`btn btn-${props.mode === 'light' ? 'success' : 'light'} mx-3 text-${props.mode === 'light' ? 'white' : 'black'}`} onClick={lowercaseOnClick}>Lowercase</button>
+                <button type="button" className={`btn btn-${props.mode === 'light' ? 'warning' : 'light'} text-${props.mode === 'light' ? 'white' : 'black'}`} onClick={capitalizeOnClick}>Captialize</button>
+                <button type="button" className={`btn btn-${props.mode === 'light' ? 'danger' : 'light'} mx-3 text-${props.mode === 'light' ? 'white' : 'black'}`} onClick={clearOnClick}>Clear Text</button>
             </div>
             <div>
                 <h2 className={`d-sm-flex my-5 text-${props.mode === 'light' ? 'dark' : 'light'}`}>Details Of Your Text</h2>
-                <h5 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{text.split(' ').length - 1} Words and {text.length} characters</h5>
+                <h5 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{wordCount} Words and {text.length} characters</h5>
+                {wordCounter()}
                 <h5 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{0.008 * text.split(' ').length} minutes to read</h5>
                 <h3 className={`my-3 text-${props.mode === 'light' ? 'dark' : 'light'}`}>Preview</h3>
-                <h6 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`} style={{ maxWidth: "100%" }}>{!text ? 'Enter Text To Preview': text}</h6>
+                <h6 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`} style={{ maxWidth: "100%", wordBreak: 'break-word' }}>{!text ? 'Enter Text To Preview' : text}</h6>
             </div>
         </div>
     )
